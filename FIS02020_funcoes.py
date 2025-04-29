ed73708caa30441c438e6d170f1e739ac7c4c6aa
laulwpes <pradolaura.lopes@gmail.com>
2025-04-29 16:34:23 -0300

    primeiro commit

---
+++ FIS02020_funcoes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 24 15:46:50 2025
+
+@author: laulwpes18
+"""
+from math import sqrt
+from temp import soma
+
+x = 30
+y = 33
+z = 43
+
+print(sqrt(z))
+
+#%%
+s = soma(x, y, z)
+print(s)
